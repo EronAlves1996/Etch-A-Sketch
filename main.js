c55c0332de8e2e44b22444f3dfafa495cac23c31
Eron Alves <eron.alves@rocketmail.com>
2022-03-30 23:37:33 -0300

    Definindo o esqueleto de funções

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-let grid = document.querySelector('#inner-frame-div');
 
-for(let i=0;i<16;i++){
+
+
+function drawGrid(n){ // n = tamanho da grade
+  let grid = document.querySelector('#inner-frame-div');
+  for(let i=0;i<16;i++){
   let intergrid = document.createElement('div');
   for(let j=0;j<16;j++){
     let subgrid = document.createElement('div');
@@ -9,4 +12,25 @@ for(let i=0;i<16;i++){
     intergrid.appendChild(subgrid);
   }
   grid.appendChild(intergrid);
+}
+}
+
+function activateColorMode(){
+
+}
+
+function activateEraser(){
+
+}
+
+function activateRainbow(){
+
+}
+
+function clearAll(){
+
+}
+
+function selectColor(){
+
 }
