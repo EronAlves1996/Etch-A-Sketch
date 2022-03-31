0161317994a6908fa0066c00fb12cfc4bab82e62
Eron Alves <eron.alves@rocketmail.com>
2022-03-30 23:42:55 -0300

    Definir função drawGrid, desenha a grade

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-
-
-
 function drawGrid(n){ // n = tamanho da grade
   let grid = document.querySelector('#inner-frame-div');
-  for(let i=0;i<16;i++){
-  let intergrid = document.createElement('div');
-  for(let j=0;j<16;j++){
-    let subgrid = document.createElement('div');
-    subgrid.className = 'grid-el';
-    subgrid.addEventListener('mouseenter', ()=>subgrid.style.backgroundColor="black");
-    intergrid.appendChild(subgrid);
+  
+  grid.innerHTML = "";
+
+  for(let i=0;i<n;i++){
+    let intergrid = document.createElement('div');
+    for(let j=0;j<n;j++){
+      let subgrid = document.createElement('div');
+      subgrid.className = 'grid-el';
+      subgrid.addEventListener('mouseenter', ()=>subgrid.style.backgroundColor="black");
+      intergrid.appendChild(subgrid);
+    }
+    grid.appendChild(intergrid);
   }
-  grid.appendChild(intergrid);
-}
+
+
 }
 
 function activateColorMode(){
